64151d81d4ebf125dee1119d33c36e626dd56fc4
joshiyash05 <joshiyash05@gmail.com>
2023-01-20 19:04:20 +0530

    feedpage and csv try

--- hacknova_hackathon/urls.py
+++ hacknova_hackathon/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',include('loginsign.urls')),
+     path('feed/',include('feedpage.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
