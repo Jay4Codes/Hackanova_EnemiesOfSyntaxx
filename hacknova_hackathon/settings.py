f6136772482279bfd1b19864eb40e8d16887074d
joshiyash05 <joshiyash05@gmail.com>
2023-01-21 06:44:52 +0530

    added csv visualize and explain feature

--- hacknova_hackathon/settings.py
+++ hacknova_hackathon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'knox',
      'loginsign',
      'feedpage',
+     'visualapp',
     
 ]
 
--- hacknova_hackathon/urls.py
+++ hacknova_hackathon/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',include('loginsign.urls')),
      path('feed/',include('feedpage.urls')),
+      path('visualapp/',include('visualapp.urls'))
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
